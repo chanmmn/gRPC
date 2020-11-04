bb19fa241eac060c6dc32629f9f4e0365418bb82
Ming Man Chan <chanmmn@hotmail.com>
2020-11-04 16:18:29 +0800

    Commit gRPC
    CommitgRPC

---
+++ ConAppgRpcCore/ConAppgRpcCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService1;
+using System;
+using System.Threading.Tasks;
+
+namespace ConAppgRpcCore
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
